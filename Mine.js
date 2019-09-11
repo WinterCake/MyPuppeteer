327d96cf67d8ce3ca09f533ca9469b68f4d6e235
alexis.hoffmann <alexis.hoffmann@itga.fr>
2019-09-11 09:24:03 +0200

    Change the tree for exemples files

--- Mine.js
+++ Mine.js
@@ -1,15 +1,16 @@
 const puppeteer = require("puppeteer");
 
-// TODO find a Link
-// find a Link - OK
-// Clientk on it - OK
-// scrape the page - 'ok'
-// reflexion sur l'IA
+var PlanetMetal = 0;
+var PlanetCrystal = 0;
+var PlanetDeuterium = 0;
+
+// Lance la function.
+startProg();
 
 // Action du script
-async function getPic() {
-  // Opening a browser with the give arg.
-  const browser = await puppeteer.launch({ headless: false });
+async function startProg() {
+  // Opening a browser with the give arg. - headless show the navigator
+  const browser = await puppeteer.launch({ headless: true });
 
   // Opening a new page
   const page = await browser.newPage();
@@ -28,25 +29,44 @@ async function getPic() {
     return title;
   });
 
-//   console.log(result);
+  console.log(result);
 
-  var ressourceSelector = "h1";
-  var anchors = "h1";
-  var ressources = await page.evaluate(ressourceSelector => {
-    anchors = Array.from(document.querySelectorAll(ressourceSelector));
-    console.log(anchors);
-    return anchors.map(anchor => { return `${anchor.p[0].innerText} - ${anchor.p[1].innerText}`;
-    });
-  }, ressources);
-
-  await console.log(ressources.join("\n"));
+  // Récupération des ressources du fichier de test
+   await GetRessources(page);
 
   // Close the browser
   await browser.close();
 }
 
-// Lance la function.
-getPic();
+
+async function GetRessources(page) {
+    //Selector for the ressources
+    var ressourceSelector = "p";
+
+    var ressources = await page.evaluate(ressourceSelector => {
+        const anchors = Array.from(document.querySelectorAll(ressourceSelector));
+        return anchors.map(anchor => {
+            const title = anchor.textContent.split("|")[0].trim();
+            return `${title}`;
+        });
+    }, ressourceSelector);
+
+    // récupération des ressources
+    for (let index = 0; index < ressources.length; index++) {
+        const element = ressources[index];
+        if (element == "Métal") 
+            PlanetMetal = ressources[index + 1];
+        if (element == "Crystal")
+            PlanetCrystal = ressources[index + 1];
+        if (element == "Deutérium")
+            PlanetDeuterium = ressources[index + 1];
+    }
+
+    //Log the ressources
+    await console.log("PlanetMetal : " + PlanetMetal);
+    await console.log("PlanetCrystal : " + PlanetCrystal);
+    await console.log("PlanetDeuterium : " + PlanetDeuterium);
+}
 
 // ################# EXEMPLE ####################
 async function MadMoiZelleScript() {
---
+++ TODO.md
@@ -0,0 +1,24 @@
+# TODO
+
+## Augmenter un niveau
+* Lire les ressources de la planète
+* Lire les couts des niveaux
+* Gérer la priorité des augmentations -> section dédié
+* Augmenter le niveau
+* Attendre ? ou dispo de la file de création ?
+
+
+## Gestions de priorité 
+
+* liste d'importance a partir du tableau des technos ? voir dans lesquels la technos est le plus cité attribué des points
+* BDD ? ou Json ? +1 pour Json pour son exportabilité / +1 BDD faciliter de gestion ?
+
+## Changement de planètes
+
+* Stocker les ressources de chaque planètes ou les relire ? 
+* Calculer la productivité et la timer pour prochaine évolution ou jouer le script régulièrement ?
+
+## Points Techniques
+
+* Utiliser plusieurs fichier JS dédié aux divers 'fonctionnalité'
+* Généré utiliser des fichiers Json
